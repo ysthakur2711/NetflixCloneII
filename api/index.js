bea1217e9da422b69a49f69a6aafdc89c4474eaf
ysthakur2711 <96758531+ysthakur2711@users.noreply.github.com>
2025-01-27 01:23:25 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,35 +2,25 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require("cors");
-
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
 const listRoute = require("./routes/lists");
+const cors = require('cors');
 
-const port = process.env.PORT || 4000;
+const port  = process.env.PORT | 4000
 
 dotenv.config();
-
-const corsOptions = {
-    origin: 'https://yogeshrajawat.netlify.app', // Allow frontend origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
-    credentials: true, // Allow cookies/auth headers
-    allowedHeaders: 'Content-Type,Authorization', // Allowed headers
-};
-
-app.use(cors(corsOptions)); // Enable CORS with options
-app.options('*', cors(corsOptions)); // Handle preflight requests
+app.use(cors()); 
 
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-    })
-    .then(() => console.log("DB Connection Successful"))
-    .catch((err) => {
-        console.error(err);
-    });
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.use(express.json());
 
@@ -39,10 +29,10 @@ app.use("/users", userRoute);
 app.use("/movies", movieRoute);
 app.use("/lists", listRoute);
 
-app.get("/", (req, res) => {
-    res.send("Hello World ji kaise ho saare!!");
-});
+app.get('/',(req,res)=>{
+  res.send("Hello World ji kaise ho saare!!");
+})
 
-app.listen(8800, () => {
-    console.log(`Backend server is running on port 8800`);
+app.listen(port, () => {
+  console.log(`Backend server is running ${port}!`);
 });
